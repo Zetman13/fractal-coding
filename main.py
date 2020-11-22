df94496d487199ac31b30d0cb2e22a43eac5fe75
Nikita Chestnov <chestnov.nn@phystech.edu>
2020-11-22 12:45:59 +0300

    Modified algorithm: added classes for blocks to increase speed. Small refactoring

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def encode_decode(img_name, output_dir, block_size):
     Path(f"{output_dir}").mkdir(parents=True, exist_ok=True)
     img = plt.imread(f"input/{img_name}.bmp")
     code = encode(img, block_size)
-    print(f"Coding size: {sys.getsizeof(code)} bytes")
     iters = decode(code, block_size, n_iters=16)
-
     for i, step in enumerate(iters):
         plt.imsave(f"{output_dir}/{img_name}_{i}.bmp", step, cmap='gray')
+    print(f"Coding size: {sys.getsizeof(code)} bytes")
     return iters
 
 
 if __name__ == '__main__':
     img_name = 'Lena'
     block_size = 8
+    print(f"Image name: {img_name}, block size: {block_size}")
     iters_2 = encode_decode(img_name, f"output/{img_name}_{block_size}", block_size)
     psnr_result = psnr(plt.imread(f"input/{img_name}.bmp"), iters_2[-1])
     print(f"PSNR: {psnr_result}")
--- src/fractal.py
+++ src/fractal.py
@@ -3,29 +3,29 @@ from numba import njit, prange
 from src.metrics import timing_val
 
 
-@timing_val
 @njit(parallel=True)
 def encode(img, block_size):
     exp_size = block_size * 2
     i_size = img.shape[0]
     j_size = img.shape[1]
     transformed_blocks = get_transformed_blocks(img, exp_size)
-    transforms = [[(0, 0, 0, 0, 0.0, 0.0) for _ in range(0, j_size, block_size)] for _ in range(0, i_size, block_size)]
+    transforms = [[(0, 0, 0, 0, 0.0, 0.0) for _ in range(j_size // block_size)] for _ in range(i_size // block_size)]
     for i in prange(i_size // block_size):
         for j in prange(j_size // block_size):
-            min_d = np.inf
-            R = img[i*block_size:i*block_size + block_size, j*block_size:j*block_size + block_size]
-            for x, y, direction, angle, D in transformed_blocks:
-                k, b = fit_contrast_brightness(R, D)
-                D = k*D + b
-                d = np.sum(np.square(R - D))
-                if d < min_d:
-                    min_d = d
-                    transforms[i][j] = (x, y, direction, angle, k, b)
+            min_diff = np.inf
+            r = img[i*block_size:i*block_size + block_size, j*block_size:j*block_size + block_size]
+            r_code = get_code(r)
+            for x, y, direction, angle, d, d_code in transformed_blocks:
+                if np.count_nonzero(r_code != d_code) == 0:
+                    k, b = fit_contrast_brightness(r, d)
+                    d = k*d + b
+                    diff = np.sum(np.square(r - d))
+                    if diff < min_diff:
+                        min_diff = diff
+                        transforms[i][j] = (x, y, direction, angle, k, b)
     return transforms
 
 
-@timing_val
 def decode(transforms, block_size, n_iters=8):
     exp_size = block_size * 2
     height = len(transforms) * block_size
@@ -35,10 +35,10 @@ def decode(transforms, block_size, n_iters=8):
     for _ in range(n_iters):
         for i in range(0, height, block_size):
             for j in prange(0, width, block_size):
-                x, y, flip, angle, k, b = transforms[i // block_size][j // block_size]
-                D = avg_pool(results[-1][x*exp_size:(x+1)*exp_size, y*exp_size:(y+1)*exp_size])
-                R = transform(D, flip, angle, k, b)
-                result[i:i+block_size, j:j+block_size] = R
+                x, y, direction, angle, k, b = transforms[i // block_size][j // block_size]
+                d = avg_pool(results[-1][x*exp_size:(x+1)*exp_size, y*exp_size:(y+1)*exp_size])
+                r = transform(d, direction, angle, k, b)
+                result[i:i+block_size, j:j+block_size] = r
         results.append(normalize(result))
     return results
 
@@ -76,22 +76,34 @@ def normalize(img):
 
 
 @njit
-def fit_contrast_brightness(R, D):
-    a1 = np.ones((D.size, 1))
-    a2 = D.copy().reshape((D.size, 1))
-    A = np.concatenate((a1, a2), axis=1)
-    b = R.flatten().astype(np.float64)
-    result, _, _, _ = np.linalg.lstsq(A, b)
+def fit_contrast_brightness(r, d):
+    a1 = np.ones((d.size, 1))
+    a2 = d.copy().reshape((d.size, 1))
+    a = np.concatenate((a1, a2), axis=1)
+    b = r.flatten().astype(np.float64)
+    result, _, _, _ = np.linalg.lstsq(a, b)
     return result[1], result[0]
 
 
 @njit
+def get_code(d):
+    mean_d = d.mean()
+    d1 = d[:d.shape[0] // 2, :d.shape[1] // 2]
+    d2 = d[:d.shape[0] // 2, d.shape[1] // 2:]
+    d3 = d[d.shape[0] // 2:, :d.shape[1] // 2]
+    d4 = d[d.shape[0] // 2:, d.shape[1] // 2:]
+    mean_d1, mean_d2, mean_d3, mean_d4 = d1.mean(), d2.mean(), d3.mean(), d4.mean()
+    return np.array((mean_d1 > mean_d, mean_d2 > mean_d, mean_d3 > mean_d, mean_d4 > mean_d))
+
+
+@njit
 def get_transformed_blocks(img, block_size):
-    candidates = [[direction, angle] for direction in [1, -1] for angle in [0, 1, 2, 3]]
+    candidates = [(direction, angle) for direction in [1, -1] for angle in [0, 1, 2, 3]]
     transformed_blocks = []
-    for x in prange((img.shape[0] - block_size) // block_size + 1):
-        for y in prange((img.shape[1] - block_size) // block_size + 1):
-            D = avg_pool(img[x*block_size:x*block_size+block_size, y*block_size:y*block_size+block_size])
+    for x in prange(img.shape[0] // block_size):
+        for y in prange(img.shape[1] // block_size):
+            d = avg_pool(img[x*block_size:x*block_size+block_size, y*block_size:y*block_size+block_size])
             for direction, angle in candidates:
-                transformed_blocks.append((x, y, direction, angle, transform(D, direction, angle)))
+                transformed_d = transform(d, direction, angle)
+                transformed_blocks.append((x, y, direction, angle, transformed_d, get_code(transformed_d)))
     return transformed_blocks
